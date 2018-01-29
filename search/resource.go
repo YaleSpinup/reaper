aeacd9c746d0313764e326d09cdbd9205a782002
E Camden Fisher <fish@fishnix.net>
2018-01-29 15:15:52 -0500

    SupportDepartmentContact should be used instead of CreatedBy for TryIT notifications

--- search/resource.go
+++ search/resource.go
@@ -2,14 +2,15 @@ package search
 
 // Resource is the resource object returned from elasticsearch
 type Resource struct {
-	Account    string
-	CreatedBy  string `json:"yale:created_by"`
-	ID         string
-	Name       string `json:"name,omitempty"`
-	Provider   string
-	Status     string
-	RenewedAt  string `json:"yale:renewed_at,omitempty"`
-	NotifiedAt string `json:"yale:notified_at,omitempty"`
-	FQDN       string `json:"yale:fqdn,omitempty"`
-	Org        string `json:"yale:org,omitempty"`
+	Account                  string
+	CreatedBy                string `json:"yale:created_by"`
+	SupportDepartmentContact string `json:"SupportDepartmentContact"`
+	ID                       string
+	Name                     string `json:"name,omitempty"`
+	Provider                 string
+	Status                   string
+	RenewedAt                string `json:"yale:renewed_at,omitempty"`
+	NotifiedAt               string `json:"yale:notified_at,omitempty"`
+	FQDN                     string `json:"yale:fqdn,omitempty"`
+	Org                      string `json:"yale:org,omitempty"`
 }
